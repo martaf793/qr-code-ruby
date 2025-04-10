a54b8e7e9b3fc0623206e7cf5de2db4b7dde419e
martaf793 <martaf@uchicago.edu>
2025-04-10 01:33:34 +0000

    final

--- qr.rb
+++ qr.rb
@@ -13,22 +13,30 @@ if user_answer!= "4"
     pp "What is the URL you would like to encode?"
     user_url= gets.chomp
     qrcode=RQRCode::QRCode.new(user_url)
+    pp "What would you like to call the PNG?"
+    user_png_name=gets.chomp
+    png=qrcode.as_png({:size => 500})
+    IO.binwrite("#{user_png_name}.png", png.to_s)
   elsif user_answer== "2"
     pp "What is the name of the wifi network?"
     user_wifi_name=gets.chomp
     pp "What is the password?"
     user_wifi_password=gets.chomp
-    qrcode=RQRCode::QRCode.new("WIFI:T:WPA;S:" + user_wifi_name + ";P:"+ user_wifi_password + ";;")
+    qrcode=RQRCode::QRCode.new("WIFI:T:WPA;S:"+ user_wifi_name + ";P:"+ user_wifi_password + ";;")
+    pp "What would you like to call the PNG?"
+    user_png_name=gets.chomp
+    png=qrcode.as_png({:size => 500})
+    IO.binwrite("#{user_png_name}.png", png.to_s)
   elsif user_answer== "3"
     pp "What is the phone number you want the code to send a text message to?"
     user_sms_number=gets.chomp  
     pp "What do you want to write in the message?"
     user_sms_message=gets.chomp 
     qrcode=RQRCode::QRCode.new("SMSTO:"+ user_sms_number + ":" + user_sms_message)
+    pp "What would you like to call the PNG?"
+    user_png_name=gets.chomp 
+    png=qrcode.as_png({ :size => 500})
+    IO.binwrite("#{user_png_name}.png", png.to_s) #HERE "#{NAME}.png"
   else pp "Didn't recognize that selection. Please try again"
   end
-  pp "What would you like to call the PNG?"
-  user_png_name=gets.chomp  
-  png=qrcode.as_png({:size => 500})
-IO.binwrite(user_png_name, png.to_s)
 end
